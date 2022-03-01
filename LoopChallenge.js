faaa9d9c733fd3ec2c12f3212121ae07917c962f
Nathan  Carpenter <96751531+NateCarp3@users.noreply.github.com>
2022-03-01 14:15:41 -0500

    Add files via upload

---
+++ LoopChallenge.js
@@ -0,0 +1,42 @@
+for (let i=0; i<20; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+    else {
+        console.log(0);
+    }
+    
+
+}
+
+for (let i=100;i>0; i--) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+    else {
+        console.log(0);
+    }
+}
+
+for (let i=4; i>=-3.5; i-=1.5){
+    console.log(i)
+}
+var i = 0;
+var sum = 0;
+for (i = 0; i<=100; i++) {
+    if (i<=100) {
+        sum = sum + i;
+    }
+    
+    
+}
+console.log(sum)
+
+var x = 1;
+prod = 1;
+for (let i=1; i<=12; i++){
+    if (i <= 12) {
+        prod = prod * i;
+    }
+}
+console.log(prod)
